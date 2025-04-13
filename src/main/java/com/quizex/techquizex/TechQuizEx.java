816b96b1063a97454d5c8900e9bf03981c7e4cb3
-show <zanzelika@yandex.ru>
2025-04-13 14:00:45 +0300

    Added started java swing maven project with pom.xml, that creates executable (from cmd) jar file

---
+++ src/main/java/com/quizex/techquizex/TechQuizEx.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.quizex.techquizex;
+
+public class TechQuizEx {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
