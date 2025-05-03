9b52f20fa60289c8ee11d490c87e445186503160
Timofei <fedotov8900@gmail.com>
2025-05-03 19:03:53 +0300

    Delete sections in AchievementsWindow.java

--- src/main/java/ui/AchievementsWindow.java
+++ src/main/java/ui/AchievementsWindow.java
@@ -15,15 +15,12 @@ public class AchievementsWindow extends JFrame {
     private JPanel achievementsPanel;   
     private DatabaseManager databasemanager;
     private int userId;
-    private static final String[] TEACHERS = {"Матстат - Перегуда А.И.", "Линейная алгебра - Сёмина Л. Г.", "С/С++- Качанов Б. В."};
-    private static String[][] ACHIEVEMENTS = new String[TEACHERS.length][TEACHERS.length*6];
+    private static String[] ACHIEVEMENTS = new String[12];
     
     private void setAchievementArr(){
         ArrayList<Achievement> arr = databasemanager.getAchievements();
-        for(int j = 0; j < 3; j++){
-            for(int i = 0; i < 18; i++){
-                ACHIEVEMENTS[j][i] = arr.get(i).getName();
-            }
+        for(int i = 0; i < 12; i++){
+            ACHIEVEMENTS[i] = arr.get(i).getName();
         }
     }
    
@@ -32,13 +29,13 @@ public class AchievementsWindow extends JFrame {
         this.userId = userId;
         setAchievementArr();
         setTitle("Достижения");
-        setSize(800, 550);
+        setSize(800, 600);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
         setLayout(new BorderLayout());
 
         JPanel topPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        JLabel label = new JLabel("Предмет - ФИО преподавателя:");
+        JLabel label = new JLabel("Все достижения");
         label.setFont(new Font("SansSerif", Font.BOLD, 14));
 
         JPanel bottomPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
@@ -52,13 +49,8 @@ public class AchievementsWindow extends JFrame {
         bottomPanel.add(backButton);
         add(bottomPanel, BorderLayout.SOUTH);
         
-        JComboBox<String> comboBox = new JComboBox<>(TEACHERS);
-        comboBox.setFont(new Font("SansSerif", Font.PLAIN, 14));
-        comboBox.setPreferredSize(new Dimension(350, 30));
-        comboBox.addActionListener(e -> updateAchievements(comboBox.getSelectedIndex()));
 
         topPanel.add(label);
-        topPanel.add(comboBox);
         add(topPanel, BorderLayout.NORTH);
 
         achievementsPanel = new JPanel();
@@ -66,33 +58,16 @@ public class AchievementsWindow extends JFrame {
         achievementsPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
         add(new JScrollPane(achievementsPanel), BorderLayout.CENTER);
 
-        updateAchievements(0); // Загрузить достижения для первого преподавателя
+        updateAchievements(); // Загрузить достижения для первого преподавателя
         setVisible(true);
     }
 
-    private void updateAchievements(int teacherIndex) {
+    private void updateAchievements() {
         JPanel panel = achievementsPanel;
         panel.removeAll();
         
-        String[] achievements = ACHIEVEMENTS[teacherIndex];
-        int left = 0;
-        int right = 0;
-        switch(teacherIndex){
-            case 0 -> {
-                left = 0;
-                right = 6;
-            }
-            case 1 -> {
-                left = 6;
-                right = 12;
-            }
-            case 2 -> {
-                left = 12;
-                right = 18;
-            }
-        }
-        for (int i = left; i < right; i++) {
-            AchievementPanel achievementPanel = new AchievementPanel(achievements[i], databasemanager.getAchievement(i + 1).getDescription() + achievements[i], false);
+        for (int i = 0; i < ACHIEVEMENTS.length; i++) {
+            AchievementPanel achievementPanel = new AchievementPanel(ACHIEVEMENTS[i], databasemanager.getAchievement(i + 1).getDescription() + ACHIEVEMENTS[i], false);
             panel.add(achievementPanel);
         }
 
